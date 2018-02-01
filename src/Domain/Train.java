b64a270fab78b2fe1d49b56068c58fd1817c5e9e
Rick <31820997+Rickvdg@users.noreply.github.com>
2018-02-01 21:43:23 +0100

    Initial commit

---
+++ src/Domain/Train.java
@@ -0,0 +1,14 @@
+package Domain;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Rick on 1-2-2018.
+ */
+public class Train {
+
+    private String id;
+    private ArrayList<Wagon> aangesloten = new ArrayList<Wagon>();
+
+
+}
---
+++ src/Domain/Wagon.java
@@ -0,0 +1,7 @@
+package Domain;
+
+/**
+ * Created by Rick on 1-2-2018.
+ */
+public class Wagon {
+}
